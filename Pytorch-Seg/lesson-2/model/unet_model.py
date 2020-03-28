c5dec5275d3ebb62d7eec025673e8d1e6239384e
Jack-Cherish <c411184003@gmail.com>
2020-03-28 20:20:24 +0800

    lesson-2

---
+++ Pytorch-Seg/lesson-2/model/unet_model.py
@@ -0,0 +1,42 @@
+""" Full assembly of the parts to form the complete network """
+"""Refer https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_model.py"""
+
+import torch.nn.functional as F
+
+from unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512, bilinear)
+        self.up2 = Up(512, 256, bilinear)
+        self.up3 = Up(256, 128, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+    
+if __name__ == '__main__':
+    net = UNet(n_channels=3, n_classes=1)
+    print(net)
